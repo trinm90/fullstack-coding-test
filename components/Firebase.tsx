a892978e43b04168f226e3f157a9823676639971
Nguyen Minh Tri <tri.nguyen@naivecat.com>
2021-07-04 18:59:03 +0700

    Change .evn to NEXT_PUBLIC

--- components/Firebase.tsx
+++ components/Firebase.tsx
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-console.log("key: " + process.env.REACT_APP_API_KEY);
+console.log("key: " + process.env.NEXT_PUBLIC_API_KEY);
 
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 console.log("key config: " + firebaseConfig.apiKey);
